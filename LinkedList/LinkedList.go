1de0e264b2e47d2d6cfe93ffde926e708d0d6c8e
Zhang, Kang <kang.zhang@emc.com>
2017-05-16 20:57:09 +0800

    add LinkedList

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -1,4 +1,5 @@
 package LinkedList
+
 //http://www.cnblogs.com/requelqi/p/3691711.html
 
 type ElementType interface{}
@@ -14,7 +15,18 @@ type LinkedList struct {
 	Head *INode // head node
 }
 
-//func NewIHost
+//NewINode return a New Node
 func NewINode(x ElementType, next *INode) *INode {
 	return &INode{x, next}
+}
+
+//NewLinkedList return a New LinkedList
+func NewLinkedList() *LinkedList {
+	head := &INode{0, nil}
+	return &LinkedList{head}
+}
+
+//IsEmpty return if a LinkedList is nil
+func (list *LinkedList) IsEmpty() bool {
+	return list.Head.next == nil
 }
