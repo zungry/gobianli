bc5ab8c06f92a0ecc6eec06260e810ea02aff747
Zhang, Kang <kang.zhang@emc.com>
2017-05-19 18:01:26 +0800

    add LinkedList

--- LinkedList/LinkedList.go
+++ LinkedList/LinkedList.go
@@ -1,5 +1,9 @@
 package LinkedList
 
+import (
+	"reflect"
+)
+
 //http://www.cnblogs.com/requelqi/p/3691711.html
 
 type ElementType interface{}
@@ -12,7 +16,7 @@ type INode struct {
 
 //LinkedList struct
 type LinkedList struct {
-	Head *INode // head node
+	Head *INode // head node, head node store the length of the LinkedList
 }
 
 //NewINode return a New Node
@@ -29,4 +33,14 @@ func NewLinkedList() *LinkedList {
 //IsEmpty return if a LinkedList is nil
 func (list *LinkedList) IsEmpty() bool {
 	return list.Head.next == nil
+}
+
+//Length return the length of the LinkedList
+func (list *LinkedList) Length() int {
+	return int(reflect.ValueOf(list.Head.X).Int()) // use reflect to manipulate objects with arbitary types.
+}
+
+//Append append the node at the end of LinkedList
+func (list *LinkedList) Append(node *INode) {
+
 }
